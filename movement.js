a69139b54eca7f49cde6110bbc3837ac134ccabc
MelonSounds <69148613+MelonSounds@users.noreply.github.com>
2021-03-11 22:03:24 -0600

    Update movement.js

--- movement.js
+++ movement.js
@@ -1,6 +1,6 @@
 var xv = 100
 
-if (xv < 99) {
+if (xv > 99) {
   console.log("[ ! Warning ] Suspicious activity detected.")
   document.getElementById("testing").innerHTML=("Testing successful");
 }
