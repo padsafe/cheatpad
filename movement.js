ff424a04a9ada623ff0365d1f459839cec43a4bb
MelonSounds <69148613+MelonSounds@users.noreply.github.com>
2021-03-11 22:11:52 -0600

    Update movement.js

--- movement.js
+++ movement.js
@@ -1,10 +1,14 @@
 var xv = 100
 var xvel = 100
 
+
+const success = "Testing Successful"
+
+
 if (xv > 99) {
-  document.getElementById("testing").innerHTML=("Testing successful");
-}
+  console.log(success);
+};
 
 if (xvel > 99) {
-  
-}
+  console.log(success);
+};
